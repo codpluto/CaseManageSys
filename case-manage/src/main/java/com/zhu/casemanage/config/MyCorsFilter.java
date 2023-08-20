06fb4499c2f50a0db7dfff12c6cc8c6fff0e9653
codpluto <61035740+codpluto@users.noreply.github.com>
2023-08-20 23:59:07 +0800

    修改了跨域配置方法

---
+++ case-manage/src/main/java/com/zhu/casemanage/config/MyCorsFilter.java
@@ -0,0 +1,34 @@
+package com.zhu.casemanage.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+@Slf4j
+public class MyCorsFilter {
+    @Bean
+    public CorsFilter CorsFilter() {
+        log.info("---进入跨域config-----");
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
+
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
--- case-manage/src/main/java/com/zhu/casemanage/config/MyWebMvcConfig.java
+++ case-manage/src/main/java/com/zhu/casemanage/config/MyWebMvcConfig.java
@@ -49,17 +49,27 @@ public class MyWebMvcConfig implements WebMvcConfigurer {
     }
 
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                //是否发送Cookie
-                .allowCredentials(true)
-                //放行哪些原始域
-                .allowedOriginPatterns("*")
-                .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
-                .allowedHeaders("*")
-                .exposedHeaders("*");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")  // 匹配所有的路径
+//                .allowCredentials(true) // 设置允许凭证
+//                .allowedHeaders("*")   // 设置请求头
+//                .allowedMethods("GET","POST","PUT","DELETE") // 设置允许的方式
+//                .allowedOriginPatterns("*");
+//    }
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        log.info("----进入跨域配置--------");
+//        registry.addMapping("/**")
+//                //是否发送Cookie
+//                .allowCredentials(true)
+//                //放行哪些原始域
+//                .allowedOriginPatterns("*")
+//                .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
+//                .allowedHeaders("*")
+//                .exposedHeaders("*");
+//    }
 
 
 }
